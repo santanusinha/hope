5293f1935cdd02409c1feb33f487f31272b2fc32
Rishabh <r0goyal@users.noreply.github.com>
2022-11-03 10:02:41 +0530

    Reverted number of forks

--- hope-lang/src/test/java/io/appform/hope/lang/BenchmarkTest.java
+++ hope-lang/src/test/java/io/appform/hope/lang/BenchmarkTest.java
@@ -32,7 +32,7 @@ public abstract class BenchmarkTest {
                 .measurementTime(TimeValue.seconds(5))
                 .measurementIterations(3)
                 .threads(1)
-                .forks(1)
+                .forks(3)
                 .shouldFailOnError(true)
                 .shouldDoGC(true)
                 .build();
